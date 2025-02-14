03416713391f0baa82bf64ab0d2fa6e09c03e54c
Eon-ts <aeonsitux@gmail.com>
2025-02-13 19:14:44 -0600

    Modelo libros

---
+++ Ejercicio1/Models/libro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Ejercicio1.Models
+{
+    public class libro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int AñoPublicacion { get; set; }
+        public int AutorId { get; set; }
+        public int CategoriaId { get; set; }
+        public string? Resumen { get; set; }
+
+    }
+}
